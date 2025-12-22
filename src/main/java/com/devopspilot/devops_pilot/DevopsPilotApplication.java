115840c1968af17061fb258f5c9b7456e5132b48
Dakarai Mahisa <mahisadakarai@gmail.com>
2025-12-22 12:34:24 +0530

    Initial Spring Boot setup with health check and clean architecture

---
+++ src/main/java/com/devopspilot/devops_pilot/DevopsPilotApplication.java
@@ -0,0 +1,13 @@
+package com.devopspilot.devops_pilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsPilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsPilotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devopspilot/devops_pilot/DevopsPilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devopspilot.devops_pilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsPilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
